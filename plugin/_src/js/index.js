4490132e518d006dd7160dafcf084d3002d034a6
emzimmer <max@emzimmer.com>
2021-06-23 18:26:34 -0700

    Include a console log for upgrade notice

--- plugin/_src/js/index.js
+++ plugin/_src/js/index.js
@@ -37,6 +37,8 @@ import '../scss/index.scss';
  */
 function RunStyleFlow() {
 
+    console.log('You are running the standalone edition of Style Flow. For even more power, consider upgrading to the Editor Enhancer suite, which includes Style Flow!');
+
     jQuery('.oxygen-sidebar-tabs').append('<div id="style-flow"></div>');
     jQuery('.oxygen-global-settings').css({'z-index':'999999999'});
 
